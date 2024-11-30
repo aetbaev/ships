b3d71cd05908056a8bc739b95af7c58077e6dadd
Timur Aetbaev <aetbaev@gmail.com>
2024-11-30 19:34:37 +0300

    Install Flowbite, postcss, and @vitejs/plugin-vue.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+
+import {createApp} from 'vue';
+import Spec from './components/Spec.vue';
+
+import 'flowbite';
+
+createApp({})
+    .component('Spec', Spec)
+    .mount('#app')
